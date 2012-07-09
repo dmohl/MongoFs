7d4def411ccff9fc0c733b7c93c5d1d19a935b11
Dan Mohl <dmohl@yahoo.com>
2012-07-08 19:10:44 -0500

    Created.

---
+++ src/MongoFs.fs
@@ -0,0 +1,26 @@
+﻿[<AutoOpen>] 
+module MongoFs
+
+open MongoDB.Driver
+open System.Configuration
+
+let createMongoServerWithConnString (connString:string) = 
+    MongoServer.Create connString
+
+let createMongoServerWithConfig (configName:string) = 
+    MongoServer.Create ConfigurationManager.AppSettings.[configName]
+
+let createMongoServer () =
+    createMongoServerWithConfig "MongoDbConnectionString"
+
+let createLocalMongoServer () =
+    createMongoServerWithConnString "mongodb://localhost/?safe=true"
+
+let getMongoDatabase (dbName:string) (mongoServer:MongoServer) =
+    mongoServer.GetDatabase dbName
+
+let getMongoCollection<'a> (collectionName:string) (db:MongoDatabase) = 
+    if not (db.CollectionExists collectionName) then
+        db.CreateCollection collectionName |> ignore
+    db.GetCollection<'a> collectionName
+
---
+++ tests/MongoFs.Tests/MongoFs.IntegrationTests.fs
@@ -0,0 +1,54 @@
+﻿module MongoFs.IntegrationTests
+
+open NUnit.Framework
+open FsUnit
+open System.Linq
+open MongoDB.Bson
+open MongoDB.Driver
+
+let dbName = "resources"
+let collectionName = "people"
+
+type Person = { 
+    mutable _id : ObjectId 
+    mutable FirstName : string 
+    mutable LastName : string 
+}
+
+let verifyAddDocument (people:MongoCollection<Person>) =
+    { _id = ObjectId.GenerateNewId(); FirstName = "John"; LastName = "Doe" }
+    |> people.Insert |> ignore
+
+    let person = people.FindOne()
+    person.FirstName |> should equal "John"
+    person.LastName |> should equal "Doe"
+    person._id |> should not' (be Null)
+
+[<Test>]
+let ``When using the default URL it should be able to create documents``() =
+    let people = 
+        createLocalMongoServer()
+        |> getMongoDatabase dbName
+        |> getMongoCollection<Person> collectionName
+    
+    people.RemoveAll() |> ignore
+
+    verifyAddDocument people
+
+    people.FindAll().ToList() |> should haveCount 1
+
+[<Test>]
+let ``When using the default app config approach and the collection already exists, things should still work``() =
+    let db = createMongoServer() |> getMongoDatabase dbName
+    db.DropCollection collectionName |> ignore
+
+    db.CollectionExists collectionName |> should not' (be True)
+
+    db |> getMongoCollection<Person> collectionName |> ignore
+    let people = db |> getMongoCollection<Person> collectionName
+
+    verifyAddDocument people
+    
+    db |> getMongoCollection<Person> collectionName |> ignore
+
+    verifyAddDocument people
